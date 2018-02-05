d5234597ea990e089b4248553be01ba08592ac37
Jeff Carbonella <jeff.carbonella@gusto.com>
2018-02-05 10:55:52 -0700

    Add tests for autostage option

--- core/config_test.go
+++ core/config_test.go
@@ -66,6 +66,7 @@ tasks:
     fix:
       command: yarn scss-lint-fix
       output: Completed
+      autostage: true
 `)
 
 	config, err := NewConfig(data)
@@ -73,6 +74,9 @@ tasks:
 	assert.Equal(t, len(config.Tasks), 1, "There should only be one task.")
 	assert.Equal(t, config.Tasks[0].Name, "SCSS Lint", "The name should be set correctly")
 	assert.Equal(t, config.Tasks[0].Command, "yarn scss-lint", "The command should be set correctly")
+	assert.Equal(t, config.Tasks[0].Fix.Command, "yarn scss-lint-fix", "The fix command should be set correctly")
+	assert.Equal(t, config.Tasks[0].Fix.Output, "Completed", "The fix output should be set correctly")
+	assert.Equal(t, config.Tasks[0].Fix.Autostage, true, "The fix autostage should be set correctly")
 	assert.Nil(t, err, "The error should not be present.")
 
 }
