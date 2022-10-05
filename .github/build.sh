3c1dd65fe781694d99d5f4ea5a7c07e4dc5a2b81
Adam Neumann <adam.neumann@gusto.com>
2022-10-05 15:48:47 -0700

    Feature: Also build for linux arm64

--- .github/build.sh
+++ .github/build.sh
@@ -2,11 +2,19 @@
 
 set -xe
 
-export GOARCH="amd64"
+# Build for darwin
 export GOOS="darwin"
+
+export GOARCH="amd64"
 /usr/local/go/bin/go build -o committer.amd64 committer.go
 
 export GOARCH="arm64"
 /usr/local/go/bin/go build -o committer.arm64 committer.go
 
 /usr/bin/lipo committer.amd64 committer.arm64 -create -output committer
+
+# Also build for linux
+export GOOS="linux"
+
+export GOARCH="arm64"
+/usr/local/go/bin/go build -o committer.linux-arm64 committer.go
